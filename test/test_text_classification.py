01f2b894269b189494617adc9be91410a5b143a3
Quester-one <2113809110@qq.com>
2022-05-06 21:27:27 +0800

    add base framework

--- test/test_text_classification.py
+++ test/test_text_classification.py
@@ -2,17 +2,20 @@ import torch
 from cogktr import *
 
 """
-reader=XXXReader()
-train_data,valid_data,test_data=reader.read_all()
-
 tagger=XXXTagger()
 linker=XXXLinker()
 searcher=XXXSearcher()
-representer=XXXRepresenter()
-enhancer=Enhancer([("tagger",tagger),("linker",linker),("searcher",searcher),("representer",representer)])
-train_data=enhancer.enhance(train_data)
-valid_data=enhancer.enhance(valid_data)
-test_data=enhancer.enhance(test_data)
+embedder=XXXEmbedder()
+enhancer=Enhancer([("tagger",tagger),("linker",linker),("searcher",searcher),("embedder",embedder)])
+
+reader=XXXReader()
+train_data=reader.read_train_data()
+valid_data=reader.read_valid_data()
+test_data=reader.read_test_data()
+
+train_data=enhancer.enhance_data(train_data)
+valid_data=enhancer.enhance_data(valid_data)
+test_data=enhancer.enhance_data(test_data)
 
 processor=XXXProcessor()
 train_dataset=processor.process(train_data)
@@ -20,6 +23,7 @@ valid_dataset=processor.process(valid_data)
 test_dataset=processor.process(test_data)
 
 model=XXXModel()
+model=enhancer.enhance_model(model)
 loss=XXXLoss()
 metric=XXXMetric()
 optimizer=XXXOptimizer()
