25d7e5cdd9f98785fe6f02c7defcc27c1c38d9ac
Quester-one <2113809110@qq.com>
2022-05-08 01:52:47 +0800

    add basemodel basetcmodel

---
+++ cogktr/models/basemodel.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def loss(self,batch,loss_function):
+        pass
+
+    def forward(self,*args):
+        pass
+
+    def evaluate(self,batch,metric_function):
+        pass
+
+    def predict(self,*args):
+        pass
