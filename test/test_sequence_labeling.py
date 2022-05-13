8225cbbec2808e1fed91675ecf6f713bafca7bde
Quester-one <2113809110@qq.com>
2022-05-13 16:28:19 +0800

    save

--- test/test_sequence_labeling.py
+++ test/test_sequence_labeling.py
@@ -15,7 +15,39 @@ dev_dataset = processor.process(dev_data)
 test_dataset = processor.process(test_data)
 
 model = BaseTextClassificationModel(plm="bert-base-cased", vocab=vocab)
-metric = BaseClassificationMetric(mode="binary")
+metric = BaseClassificationMetric(mode="multi")
 loss = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+trainer = Trainer(model,
+                  train_dataset,
+                  dev_data=dev_dataset,
+                  n_epochs=20,
+                  batch_size=50,
+                  loss=loss,
+                  optimizer=optimizer,
+                  scheduler=None,
+                  metrics=metric,
+                  train_sampler=None,
+                  dev_sampler=None,
+                  drop_last=False,
+                  gradient_accumulation_steps=1,
+                  num_workers=5,
+                  save_path=None,
+                  save_file=None,
+                  print_every=None,
+                  scheduler_steps=None,
+                  validate_steps=None,
+                  save_steps=None,
+                  grad_norm=1,
+                  use_tqdm=True,
+                  device=device,
+                  device_ids=[4],
+                  callbacks=None,
+                  metric_key=None,
+                  writer_path=None,
+                  fp16=False,
+                  fp16_opt_level='O1',
+                  logger_path=None)
+trainer.train()
 print("end")
