fe3813712b17ce79c143d129ce53258edef6a574
HongbangYuan <1208314139@qq.com>
2022-05-29 18:20:31 +0800

    add evaluator

--- test/test_sentence_pair_classification.py
+++ test/test_sentence_pair_classification.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from cogktr import *
+from cogktr.core.evaluator import Evaluator
 
-torch.cuda.set_device(4)
-device = torch.device('cuda:4')
+torch.cuda.set_device(2)
+device = torch.device('cuda:2')
 
 reader = QNLIReader(raw_data_path="/data/mentianyi/code/CogKTR/datapath/sentence_pair/QNLI/raw_data")
 train_data, dev_data, test_data = reader.read_all()
@@ -19,35 +20,51 @@ metric = BaseClassificationMetric(mode="binary")
 loss = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
-trainer = Trainer(model,
-                  train_dataset,
-                  dev_data=dev_dataset,
-                  n_epochs=20,
-                  batch_size=50,
-                  loss=loss,
-                  optimizer=optimizer,
-                  scheduler=None,
-                  metrics=metric,
-                  train_sampler=None,
-                  dev_sampler=None,
-                  drop_last=False,
-                  gradient_accumulation_steps=1,
-                  num_workers=5,
-                  save_path=None,
-                  save_file=None,
-                  print_every=None,
-                  scheduler_steps=None,
-                  validate_steps=None,
-                  save_steps=None,
-                  grad_norm=1,
-                  use_tqdm=True,
-                  device=device,
-                  device_ids=[4],
-                  callbacks=None,
-                  metric_key=None,
-                  writer_path=None,
-                  fp16=False,
-                  fp16_opt_level='O1',
-                  logger_path=None)
-trainer.train()
-print("end")
+evaluator = Evaluator(
+    model=model,
+    checkpoint_path="/data/hongbang/CogKTR/datapath/sentence_pair/QNLI/train/2022-05-29-09:02:48/checkpoint-100",
+    dev_data=dev_dataset,
+    metrics=metric,
+    sampler=None,
+    drop_last=False,
+    collate_fn=None,
+    file_name="models.pt",
+    batch_size=32,
+    device=device,
+    user_tqdm=True,
+)
+evaluator.evaluate()
+print("End")
+
+# trainer = Trainer(model,
+#                   train_dataset,
+#                   dev_data=dev_dataset,
+#                   n_epochs=20,
+#                   batch_size=50,
+#                   loss=loss,
+#                   optimizer=optimizer,
+#                   scheduler=None,
+#                   metrics=metric,
+#                   train_sampler=None,
+#                   dev_sampler=None,
+#                   drop_last=False,
+#                   gradient_accumulation_steps=1,
+#                   num_workers=5,
+#                   save_path="/data/hongbang/CogKTR/datapath/sentence_pair/QNLI/",
+#                   save_file=None,
+#                   print_every=None,
+#                   scheduler_steps=None,
+#                   validate_steps=100,
+#                   save_steps=1,
+#                   grad_norm=1,
+#                   use_tqdm=True,
+#                   device=device,
+#                   device_ids=[4],
+#                   callbacks=None,
+#                   metric_key=None,
+#                   writer_path=None,
+#                   fp16=False,
+#                   fp16_opt_level='O1',
+#                   logger_path=None)
+# trainer.train()
+# print("end")
