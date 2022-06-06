91c59dbf1abbe65c57e18e6f7ccaf967d0a8f695
Quester-one <2113809110@qq.com>
2022-06-07 02:00:15 +0800

    add knowledge downloader

--- cogktr/utils/download_utils.py
+++ cogktr/utils/download_utils.py
@@ -74,7 +74,5 @@ if __name__ == "__main__":
     downloader.download_squad2_raw_data("/data/mentianyi/code/CogKTR/datapath/reading_comprehension/SQuAD2.0/raw_data")
     downloader.download_sst2_raw_data("/data/mentianyi/code/CogKTR/datapath/text_classification/SST_2/raw_data")
     downloader.download_stsb_raw_data("/data/mentianyi/code/CogKTR/datapath/sentence_pair/STS_B/raw_data")
-    downloader.download_wikipedia2vec_knowledge(
-        "/data/mentianyi/code/CogKTR/datapath/knowledge_graph/wikipedia2vec/raw_data")
-    downloader.download_wikipedia_entity_description_knowledge(
-        "/data/mentianyi/code/CogKTR/datapath/knowledge_graph/wikipedia/raw_data")
+    downloader.download_wikipedia2vec_knowledge("/data/mentianyi/code/CogKTR/datapath/knowledge_graph/wikipedia2vec/raw_data")
+    downloader.download_wikipedia_entity_description_knowledge("/data/mentianyi/code/CogKTR/datapath/knowledge_graph/wikipedia/raw_data")
