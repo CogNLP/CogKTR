aa2c125fb1c98c62a720d08fdd2b77f69585507e
HongbangYuan <1208314139@qq.com>
2022-06-07 15:35:42 +0800

    update move dict to  device

--- cogktr/utils/general_utils.py
+++ cogktr/utils/general_utils.py
@@ -10,13 +10,17 @@ def move_dict_value_to_device(batch,device,rank=-1,non_blocking=False):
     if rank == -1 and not isinstance(device, torch.device):
         raise TypeError(f"device must be `torch.device` in a single process, got `{type(device)}`")
 
+    _move_dict_value_to_device(batch,device,rank,non_blocking)
+
+def _move_dict_value_to_device(batch,device,rank=-1,non_blocking=False):
     for key, value in batch.items():
         if isinstance(value, torch.Tensor):
             if rank == -1:
-                batch[key] = value.to(device)
+                batch[key] = value.to(device,non_blocking=non_blocking)
             else:
-                batch[key] = value.to(torch.device("cuda:{}".format(rank)))
-
+                batch[key] = value.to(torch.device("cuda:{}".format(rank)),non_blocking=non_blocking)
+        elif isinstance(value,dict):
+            _move_dict_value_to_device(batch[key],device,rank,non_blocking)
 
 def reduce_mean(tensor,nprocs):
     rt = tensor.clone()
