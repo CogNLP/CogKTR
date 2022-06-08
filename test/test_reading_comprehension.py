c1f47597667878f62f7af144125b3178a71de000
HongbangYuan <1208314139@qq.com>
2022-06-08 23:20:26 +0800

    add reading comprehension test program

--- test/test_reading_comprehension.py
+++ test/test_reading_comprehension.py
@@ -2,11 +2,60 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from cogktr import *
+from cogktr.utils.general_utils import init_cogktr
+from cogktr.data.processor.squad2_processors.squad2_processor import Squad2Processor
+from cogktr.models.base_reading_comprehension_model import BaseReadingComprehensionModel
 
-torch.cuda.set_device(4)
-device = torch.device('cuda:4')
+device,output_path = init_cogktr(
+    device_id=4,
+    output_path="/data/hongbang/CogKTR/datapath/reading_comprehension/SQuAD2.0/experimental_result/",
+    folder_tag="debug_metric",
+)
 
 reader = Squad2Reader(raw_data_path="/data/mentianyi/code/CogKTR/datapath/reading_comprehension/SQuAD2.0/raw_data")
-train_data, dev_data, test_data = reader.read_all()
+train_data, dev_data, _ = reader.read_all()
 vocab = reader.read_vocab()
+
+processor = Squad2Processor(plm="bert-base-cased", max_token_len=256, vocab=vocab)
+train_dataset = processor.process_train(train_data)
+dev_dataset = processor.process_dev(dev_data)
+
+model = BaseReadingComprehensionModel(plm="bert-base-cased",vocab=vocab)
+metric = BaseClassificationMetric(mode="multi")
+loss = nn.CrossEntropyLoss(ignore_index=256)
+optimizer = optim.Adam(model.parameters(), lr=0.00001)
+
+trainer = Trainer(model,
+                  train_dataset,
+                  dev_data=dev_dataset,
+                  n_epochs=20,
+                  batch_size=50,
+                  loss=loss,
+                  optimizer=optimizer,
+                  scheduler=None,
+                  metrics=metric,
+                  train_sampler=None,
+                  dev_sampler=None,
+                  drop_last=False,
+                  gradient_accumulation_steps=1,
+                  num_workers=5,
+                  print_every=None,
+                  scheduler_steps=None,
+                  validate_steps=500,
+                  save_steps=None,
+                  output_path=output_path,
+                  grad_norm=1,
+                  use_tqdm=True,
+                  device=device,
+                  callbacks=None,
+                  metric_key=None,
+                  fp16=False,
+                  fp16_opt_level='O1',
+                  )
+trainer.train()
 print("end")
+
+
+
+
+
