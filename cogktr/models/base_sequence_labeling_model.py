07b44fcf057cd77c323c0e3084f112cbf33fc1c9
Quester-one <2113809110@qq.com>
2022-06-09 04:23:53 +0800

    add base_sequence_labeling_model

--- cogktr/models/base_sequence_labeling_model.py
+++ cogktr/models/base_sequence_labeling_model.py
@@ -5,6 +5,26 @@ import torch.nn.functional as F
 import torch
 
 
+def bio_tag_to_spans(tags, ignore_labels=None):
+    ignore_labels = set(ignore_labels) if ignore_labels else set()
+
+    spans = []
+    prev_bio_tag = None
+    for idx, tag in enumerate(tags):
+        tag = tag.lower()
+        bio_tag, label = tag[:1], tag[2:]
+        if bio_tag == 'b':
+            spans.append((label, [idx, idx]))
+        elif bio_tag == 'i' and prev_bio_tag in ('b', 'i') and label == spans[-1][0]:
+            spans[-1][1][1] = idx
+        elif bio_tag == 'o':  # o tag does not count
+            pass
+        else:
+            spans.append((label, [idx, idx]))
+        prev_bio_tag = bio_tag
+    return [(span[0], (span[1][0], span[1][1] + 1)) for span in spans if span[0] not in ignore_labels]
+
+
 class BaseSequenceLabelingModel(BaseModel):
     def __init__(self, vocab, plm):
         super().__init__()
@@ -12,30 +32,84 @@ class BaseSequenceLabelingModel(BaseModel):
         self.plm = plm
 
         self.bert = BertModel.from_pretrained(plm)
-        # TODO: auto select input size
         self.input_size = 768
-        self.classes_num = len(vocab["label_vocab"])
+        self.classes_num = len(vocab["ner_label_vocab"])
         self.linear = nn.Linear(in_features=self.input_size, out_features=self.classes_num)
 
     def loss(self, batch, loss_function):
-        batch_len = len(batch)
-        token, label = batch
-        pred = self.forward(token=token)
-        loss = loss_function(pred, label) / batch_len
+        input_ids, attention_masks, segment_ids, valid_masks, label_ids, label_masks, words_len = self.get_batch(batch)
+        pred = self.forward(input_ids=input_ids,
+                            attention_masks=attention_masks,
+                            segment_ids=segment_ids,
+                            valid_masks=valid_masks)
+        active_loss = label_masks.view(-1) == 1
+        active_pred = pred.view(-1, len(self.vocab["ner_label_vocab"]))[active_loss]
+        active_labels = label_ids.view(-1)[active_loss]
+        loss = loss_function(active_pred, active_labels)
         return loss
 
-    def forward(self, token):
-        x = self.bert(token).pooler_output
-        x = self.linear(x)
-        return x
+    def forward(self, input_ids, attention_masks, segment_ids, valid_masks):
+        current_device = input_ids.device
+        x = self.bert(input_ids=input_ids, attention_mask=attention_masks, token_type_ids=segment_ids).last_hidden_state
+        batch_size, max_token_len, embedding_dim = x.shape
+        valid_x = torch.zeros(batch_size, max_token_len, embedding_dim, dtype=torch.float, device=current_device)
+        for i in range(batch_size):
+            pos = 0
+            for j in range(max_token_len):
+                if valid_masks[i][j].item() == 1:
+                    valid_x[i][pos] = x[i][j]
+                    pos += 1
+        valid_x = self.linear(valid_x)
+        return valid_x
 
     def evaluate(self, batch, metric_function):
-        token, label = batch
-        pred = self.predict(token)
-        metric_function.evaluate(pred, label)
+        input_ids, attention_masks, segment_ids, valid_masks, label_ids, label_masks, words_len = self.get_batch(batch)
+        pred_list = self.predict(input_ids=input_ids,
+                                 attention_masks=attention_masks,
+                                 segment_ids=segment_ids,
+                                 valid_masks=valid_masks,
+                                 words_len=words_len)
+        label_ids_list = label_ids.tolist()
+        for i, item in enumerate(label_ids_list):
+            label_ids_list[i] = [self.vocab["ner_label_vocab"].id2label(id) for id in item[1:words_len[i] + 1]]
+        pred_eval = []
+        label_eval = []
+        for pred_item, label_ids_item in zip(pred_list, label_ids_list):
+            pred_spans = bio_tag_to_spans(tags=pred_item)
+            gold_spans = bio_tag_to_spans(tags=label_ids_item)
+            for span in pred_spans:
+                if span in gold_spans:
+                    pred_eval.append(1)
+                    label_eval.append(1)
+                    gold_spans.remove(span)
+                else:
+                    pred_eval.append(1)
+                    label_eval.append(0)
+            for span in gold_spans:
+                pred_eval.append(0)
+                label_eval.append(1)
+        pred_eval = torch.tensor(pred_eval)
+        label_eval = torch.tensor(label_eval)
+        metric_function.evaluate(pred_eval, label_eval)
 
-    def predict(self, token):
-        pred = self.forward(token)
-        pred = F.softmax(pred, dim=1)
-        pred = torch.max(pred, dim=1)[1]
-        return pred
+    def predict(self, input_ids, attention_masks, segment_ids, valid_masks, words_len):
+        pred = self.forward(input_ids=input_ids,
+                            attention_masks=attention_masks,
+                            segment_ids=segment_ids,
+                            valid_masks=valid_masks)
+        pred = F.softmax(pred, dim=2)
+        pred = torch.max(pred, dim=2)[1]
+        pred_list = pred.tolist()
+        for i, item in enumerate(pred_list):
+            pred_list[i] = [self.vocab["ner_label_vocab"].id2label(id) for id in item[1:words_len[i] + 1]]
+        return pred_list
+
+    def get_batch(self, batch):
+        input_ids = batch["input_ids"]
+        attention_masks = batch["attention_masks"]
+        segment_ids = batch["segment_ids"]
+        valid_masks = batch["valid_masks"]
+        label_ids = batch["label_ids"]
+        label_masks = batch["label_masks"]
+        words_len = batch["words_len"]
+        return input_ids, attention_masks, segment_ids, valid_masks, label_ids, label_masks, words_len
