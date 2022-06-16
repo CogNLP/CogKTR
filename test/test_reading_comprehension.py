06ab2c38f67141b128ff4045353273c1cce98d1d
HongbangYuan <1208314139@qq.com>
2022-06-16 15:52:55 +0800

    add test mrc

--- test/test_reading_comprehension.py
+++ test/test_reading_comprehension.py
@@ -3,25 +3,26 @@ import torch.nn as nn
 import torch.optim as optim
 from cogktr import *
 from cogktr.utils.general_utils import init_cogktr
+from cogktr.core.metric.base_reading_comprehension_metric import BaseMRCMetric
 from cogktr.data.processor.squad2_processors.squad2_processor import Squad2Processor
 from cogktr.models.base_reading_comprehension_model import BaseReadingComprehensionModel
 
 device,output_path = init_cogktr(
-    device_id=4,
+    device_id=2,
     output_path="/data/hongbang/CogKTR/datapath/reading_comprehension/SQuAD2.0/experimental_result/",
-    folder_tag="debug_metric",
+    folder_tag="mrc_metric",
 )
 
 reader = Squad2Reader(raw_data_path="/data/mentianyi/code/CogKTR/datapath/reading_comprehension/SQuAD2.0/raw_data")
 train_data, dev_data, _ = reader.read_all()
 vocab = reader.read_vocab()
 
-processor = Squad2Processor(plm="bert-base-cased", max_token_len=256, vocab=vocab)
+processor = Squad2Processor(plm="bert-base-cased", max_token_len=256, vocab=vocab,debug=False)
 train_dataset = processor.process_train(train_data)
 dev_dataset = processor.process_dev(dev_data)
 
 model = BaseReadingComprehensionModel(plm="bert-base-cased",vocab=vocab)
-metric = BaseClassificationMetric(mode="multi")
+metric = BaseMRCMetric()
 loss = nn.CrossEntropyLoss(ignore_index=256)
 optimizer = optim.Adam(model.parameters(), lr=0.00001)
 
@@ -29,7 +30,7 @@ trainer = Trainer(model,
                   train_dataset,
                   dev_data=dev_dataset,
                   n_epochs=20,
-                  batch_size=50,
+                  batch_size=64,
                   loss=loss,
                   optimizer=optimizer,
                   scheduler=None,
