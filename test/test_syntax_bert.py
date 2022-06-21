a2b434ba198a95b551df74f32828d1b28ba20553
Quester-one <2113809110@qq.com>
2022-06-22 06:34:43 +0800

    add syntax test

--- test/test_syntax_bert.py
+++ test/test_syntax_bert.py
@@ -5,30 +5,29 @@ from cogktr import *
 from cogktr.utils.general_utils import init_cogktr
 
 device, output_path = init_cogktr(
-    device_id=7,
-    output_path="/data/mentianyi/code/CogKTR/datapath/sentence_pair/QNLI/experimental_result/",
-    folder_tag="test_dict_input",
+    device_id=3,
+    output_path="/data/mentianyi/code/CogKTR/datapath/text_classification/SST_2/experimental_result",
+    folder_tag="simple_test",
 )
 
-reader = QnliReader(raw_data_path="/data/mentianyi/code/CogKTR/datapath/sentence_pair/QNLI/raw_data")
+reader = Sst2Reader(raw_data_path="/data/mentianyi/code/CogKTR/datapath/text_classification/SST_2/raw_data")
 train_data, dev_data, test_data = reader.read_all()
 vocab = reader.read_vocab()
 
-enhancer = Enhancer(return_syntax=True,
-                    reprocess=True,
-                    save_file_name="pre_enhanced_data",
+enhancer = Enhancer(reprocess=False,
+                    save_file_name="syntax_enhanced_data",
                     datapath="/data/mentianyi/code/CogKTR/datapath",
-                    enhanced_data_path="/data/mentianyi/code/CogKTR/datapath/sentence_pair/QNLI/enhanced_data")
-# enhanced_train_dict = enhancer.enhance_train(train_data, enhanced_key_1="sentence", enhanced_key_2="question")
-enhanced_dev_dict = enhancer.enhance_dev(dev_data, enhanced_key_1="sentence", enhanced_key_2="question")
-# enhanced_test_dict = enhancer.enhance_test(test_data, enhanced_key_1="sentence", enhanced_key_2="question")
+                    enhanced_data_path="/data/mentianyi/code/CogKTR/datapath/text_classification/SST_2/enhanced_data")
+# enhanced_train_dict = enhancer.enhance_train(train_data)
+enhanced_dev_dict = enhancer.enhance_dev(dev_data)
+# enhanced_test_dict = enhancer.enhance_test(test_data)
 
-processor = QnliForSyntaxBertProcessor(plm="bert-base-cased", max_token_len=256, vocab=vocab)
+processor = Sst2ForSyntaxBertProcessor(plm="bert-base-cased", max_token_len=128, n_mask=2, vocab=vocab)
 train_dataset = processor.process_train(dev_data, enhanced_dict=enhanced_dev_dict)
 dev_dataset = processor.process_dev(dev_data, enhanced_dict=enhanced_dev_dict)
-# test_dataset = processor.process_test(test_data)
+# test_dataset = processor.process_test(test_data, enhanced_dict=enhanced_test_dict)
 
-model = BaseSentencePairClassificationModel(plm="bert-base-cased", vocab=vocab)
+model = SyntaxAttentionModel(plm="bert-base-cased", vocab=vocab)
 metric = BaseClassificationMetric(mode="binary")
 loss = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.00001)
@@ -37,7 +36,7 @@ trainer = Trainer(model,
                   train_dataset,
                   dev_data=dev_dataset,
                   n_epochs=20,
-                  batch_size=32,
+                  batch_size=50,
                   loss=loss,
                   optimizer=optimizer,
                   scheduler=None,
@@ -49,8 +48,8 @@ trainer = Trainer(model,
                   num_workers=5,
                   print_every=None,
                   scheduler_steps=None,
-                  validate_steps=None,
-                  save_steps=None,
+                  validate_steps=100,
+                  save_steps=100,
                   output_path=output_path,
                   grad_norm=1,
                   use_tqdm=True,
