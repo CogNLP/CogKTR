5d5f84e1677b346f7f80555e26091406e9a9c27c
Quester-one <2113809110@qq.com>
2022-06-25 20:55:04 +0800

    add config and update modelname

--- cogktr/data/processor/sst2_processors/sst2_for_base_text_classification_processor.py
+++ cogktr/data/processor/sst2_processors/sst2_for_base_text_classification_processor.py
@@ -8,7 +8,7 @@ from cogktr.data.processor.base_processor import BaseProcessor
 transformers.logging.set_verbosity_error()  # set transformers logging level
 
 
-class Sst2Processor(BaseProcessor):
+class Sst2ForBaseTextClassificationProcessor(BaseProcessor):
     def __init__(self, plm, max_token_len, vocab):
         super().__init__()
         self.plm = plm
@@ -16,7 +16,7 @@ class Sst2Processor(BaseProcessor):
         self.vocab = vocab
         self.tokenizer = BertTokenizer.from_pretrained(plm)
 
-    def process_train(self, data):
+    def _process(self, data):
         datable = DataTable()
         print("Processing data...")
         for sentence, label in tqdm(zip(data['sentence'], data['label']), total=len(data['sentence'])):
@@ -26,15 +26,11 @@ class Sst2Processor(BaseProcessor):
             datable("label", self.vocab["label_vocab"].label2id(label))
         return DataTableSet(datable)
 
+    def process_train(self, data):
+        return self._process(data)
+
     def process_dev(self, data):
-        datable = DataTable()
-        print("Processing data...")
-        for sentence, label in tqdm(zip(data['sentence'], data['label']), total=len(data['sentence'])):
-            token = self.tokenizer.encode(text=sentence, truncation=True, padding="max_length", add_special_tokens=True,
-                                          max_length=self.max_token_len)
-            datable("token", token)
-            datable("label", self.vocab["label_vocab"].label2id(label))
-        return DataTableSet(datable)
+        return self._process(data)
 
     def process_test(self, data):
         datable = DataTable()
@@ -53,7 +49,7 @@ if __name__ == "__main__":
     train_data, dev_data, test_data = reader.read_all()
     vocab = reader.read_vocab()
 
-    processor = Sst2Processor(plm="bert-base-cased", max_token_len=128, vocab=vocab)
+    processor = Sst2ForBaseTextClassificationProcessor(plm="bert-base-cased", max_token_len=128, vocab=vocab)
     train_dataset = processor.process_train(train_data)
     dev_dataset = processor.process_dev(dev_data)
     test_dataset = processor.process_test(test_data)
