877cdb50e7ed55471ffd4fa21d62f1e2063be542
Quester-one <2113809110@qq.com>
2022-07-04 11:06:58 +0800

    update CommonsenseqaQagnnReader

--- cogktr/data/reader/commonsenseqa_qagnn_reader.py
+++ cogktr/data/reader/commonsenseqa_qagnn_reader.py
@@ -5,6 +5,7 @@ from cogktr.utils.vocab_utils import Vocabulary
 from cogktr.utils.download_utils import Downloader
 import numpy as np
 import json
+import pickle
 
 
 class CommonsenseqaQagnnReader(BaseReader):
@@ -19,6 +20,9 @@ class CommonsenseqaQagnnReader(BaseReader):
         self.train_adj_file = 'graph/train.graph.adj.pk'
         self.dev_adj_file = 'graph/dev.graph.adj.pk'
         self.test_adj_file = 'graph/test.graph.adj.pk'
+        self.train_adj_cache_file = 'graph/train.graph.adj.pk.loaded_cache'
+        self.dev_adj_cache_file = 'graph/dev.graph.adj.pk.loaded_cache'
+        self.test_adj_cache_file = 'graph/test.graph.adj.pk.loaded_cache'
         self.ent_emb_file = 'cpnet/tzw.ent.npy'
         self.train_path = os.path.join(raw_data_path, self.train_file)
         self.dev_path = os.path.join(raw_data_path, self.dev_file)
@@ -26,6 +30,9 @@ class CommonsenseqaQagnnReader(BaseReader):
         self.train_adj_path = os.path.join(raw_data_path, self.train_adj_file)
         self.dev_adj_path = os.path.join(raw_data_path, self.dev_adj_file)
         self.test_adj_path = os.path.join(raw_data_path, self.test_adj_file)
+        self.train_adj_cache_path = os.path.join(raw_data_path, self.train_adj_cache_file)
+        self.dev_adj_cache_path = os.path.join(raw_data_path, self.dev_adj_cache_file)
+        self.test_adj_cache_path = os.path.join(raw_data_path, self.test_adj_cache_file)
         self.ent_emb_path = os.path.join(raw_data_path, self.ent_emb_file)
         self.label_vocab = Vocabulary()
         self.addition = {}
@@ -34,7 +41,9 @@ class CommonsenseqaQagnnReader(BaseReader):
         datable = DataTable()
         with open(path, "r", encoding="utf-8") as file:
             lines = file.readlines()
-        for line in lines:
+        with open(adj_path, 'rb') as adj_file:
+            adj_concept_pairs = pickle.load(adj_file)
+        for line, adj_concept_pair in zip(lines, adj_concept_pairs):
             line_dict = json.loads(line)
             example_id = line_dict["id"]
             answer_label = line_dict["answerKey"]
@@ -42,10 +51,20 @@ class CommonsenseqaQagnnReader(BaseReader):
             candidate_text_list = [label_dict["text"] for label_dict in line_dict["question"]["choices"]]
             candidate_num = len(candidate_text_list)
             context = [line_dict["question"]["stem"]] * candidate_num
+            adj = adj_concept_pair["adj"]
+            concepts = adj_concept_pair["concepts"]
+            qmask = adj_concept_pair["qmask"]
+            amask = adj_concept_pair["amask"]
+            cid2score = adj_concept_pair["cid2score"]
             datable("example_id", example_id)
             datable("answer_label", answer_label)
             datable("context", context)
             datable("candidate_text_list", candidate_text_list)
+            datable("adj", adj)
+            datable("concepts", concepts)
+            datable("qmask", qmask)
+            datable("amask", amask)
+            datable("cid2score", cid2score)
             self.label_vocab.add_sequence(candidate_character_list)
         return datable
 
@@ -59,16 +78,28 @@ class CommonsenseqaQagnnReader(BaseReader):
         datable = DataTable()
         with open(path, "r", encoding="utf-8") as file:
             lines = file.readlines()
-        for line in lines:
+        with open(adj_path, 'rb') as adj_file:
+            adj_concept_pairs = pickle.load(adj_file)
+        for line, adj_concept_pair in zip(lines, adj_concept_pairs):
             line_dict = json.loads(line)
             example_id = line_dict["id"]
             candidate_character_list = [label_dict["label"] for label_dict in line_dict["question"]["choices"]]
             candidate_text_list = [label_dict["text"] for label_dict in line_dict["question"]["choices"]]
             candidate_num = len(candidate_text_list)
             context = [line_dict["question"]["stem"]] * candidate_num
+            adj = adj_concept_pair["adj"]
+            concepts = adj_concept_pair["concepts"]
+            qmask = adj_concept_pair["qmask"]
+            amask = adj_concept_pair["amask"]
+            cid2score = adj_concept_pair["cid2score"]
             datable("example_id", example_id)
             datable("context", context)
             datable("candidate_text_list", candidate_text_list)
+            datable("adj", adj)
+            datable("concepts", concepts)
+            datable("qmask", qmask)
+            datable("amask", amask)
+            datable("cid2score", cid2score)
             self.label_vocab.add_sequence(candidate_character_list)
         return datable
 
@@ -81,9 +112,28 @@ class CommonsenseqaQagnnReader(BaseReader):
         self.label_vocab.create()
         return {"label_vocab": self.label_vocab}
 
+    def _read_cache(self, path):
+        with open(path, 'rb') as file:
+            adj_lengths_ori, concept_ids, node_type_ids, node_scores, adj_lengths, edge_index, edge_type, half_n_rel = pickle.load(
+                file)
+            adj_cache = {}
+            adj_cache["adj_lengths_ori"] = adj_lengths_ori
+            adj_cache["concept_ids"] = concept_ids
+            adj_cache["node_type_ids"] = node_type_ids
+            adj_cache["node_scores"] = node_scores
+            adj_cache["adj_lengths"] = adj_lengths
+            adj_cache["edge_index"] = edge_index
+            adj_cache["edge_type"] = edge_type
+            adj_cache["half_n_rel"] = half_n_rel
+        return adj_cache
+
     def read_addition(self):
         cp_emb = np.load(self.ent_emb_path)
         self.addition["cp_emb"] = cp_emb
+        print("Reading cache ...")
+        self.addition["train_adj_cache"] = self._read_cache(self.train_adj_cache_path)
+        self.addition["dev_adj_cache"] = self._read_cache(self.dev_adj_cache_path)
+        self.addition["test_adj_cache"] = self._read_cache(self.test_adj_cache_path)
         return self.addition
 
 
