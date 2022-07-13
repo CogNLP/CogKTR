4cf2a657fb7e808d20eaa0fa8b4bd42979d3c5d2
HongbangYuan <1208314139@qq.com>
2022-07-13 22:34:25 +0800

    update openbookqa processor

--- cogktr/data/processor/openbookqa_processors/openbookqa_for_safe_processor.py
+++ cogktr/data/processor/openbookqa_processors/openbookqa_for_safe_processor.py
@@ -1,6 +1,6 @@
 from cogktr.data.datable import DataTable
 from cogktr.data.datableset import DataTableSet
-from transformers import BertTokenizer
+from transformers import RobertaTokenizer
 from tqdm import tqdm
 import transformers
 from cogktr.data.processor.base_processor import BaseProcessor
@@ -18,7 +18,7 @@ class OpenBookQAForSafeProcessor(BaseProcessor):
         self.plm = plm
         self.max_token_len = max_token_len
         self.vocab = vocab
-        self.tokenizer = BertTokenizer.from_pretrained(plm)
+        self.tokenizer = RobertaTokenizer.from_pretrained(plm)
         self.debug = debug
         self.meta_path_set = set()
 
@@ -26,12 +26,29 @@ class OpenBookQAForSafeProcessor(BaseProcessor):
         datable = DataTable()
         data = self.debug_process(data)
         print("Processing data...")
-        for i in tqdm(range(len(data["statement"]))):
-            id,stem,answer_text,key,statement,answerKey = data[i]
-            data_dict = enhanced_data_dict[statement]
-            meta_path_feature,meta_path_count = encode_meta_path(
-                data_dict["meta_paths_list"],data_dict["meta_paths_set"],self.vocab["metapath"]
-            )
+        num_choices = 4
+        for i in tqdm(range(len(data["statement"])//num_choices)):
+            for j in range(num_choices):
+                k = num_choices * i + j
+                id,stem,answer_text,key,statement,answerKey = data[k]
+                data_dict = enhanced_data_dict[statement]
+                meta_path_feature,meta_path_count = encode_meta_path(
+                    data_dict["meta_paths_list"],data_dict["meta_paths_set"],self.vocab["metapath"]
+                )
+
+                tokenized_data = self.tokenizer.encode_plus(text=stem, text_pair=answer_text,
+                                                            truncation='longest_first',
+                                                            padding="max_length",
+                                                            add_special_tokens=True,
+                                                            return_token_type_ids=True,
+                                                            return_special_tokens_mask=True,
+                                                            max_length=self.max_token_len)
+                input_ids = tokenized_data["input_ids"]
+                input_mask = tokenized_data["input_mask"]
+                segment_ids = tokenized_data["segment_ids"]
+                output_mask = tokenized_data["output_mask"]
+            # stack the choice features together to create one sample
+
 
             # datable("input_ids", dict_data["input_ids"])
             # datable("input_mask", dict_data["input_mask"])
@@ -72,6 +89,52 @@ def encode_meta_path(meta_path_list,meta_path_set,meta_path_vocab):
         meta_path_count[idx] = meta_path_list.count(meta_path)
     return meta_path_feature,meta_path_count
 
+def process_roberta(text_a,text_b,tokenizer,max_token_length):
+    sep_token = '</s>'
+    sequence_a_segment_id = 0
+    sequence_b_segment_id = 0
+    cls_token = '<s>'
+    cls_token_segment_id = 0
+    pad_token = 0
+    pad_token_segment_id = 0
+
+
+    tokens_a = tokenizer.tokenize(text_a)
+    tokens_b = tokenizer.tokenize(text_b)
+
+    special_tokens_count = 4
+    _truncate_seq_pair(tokens_a,tokens_b,max_token_length - special_tokens_count)
+
+    tokens = tokens_a + sep_token + sep_token
+    segment_ids = [sequence_a_segment_id] * len(tokens)
+    tokens += tokens_b + sep_token
+    segment_ids += [sequence_b_segment_id] * (len(tokens_b) + 1)
+    tokens = [cls_token] + tokens
+    segment_ids = [cls_token_segment_id] + segment_ids
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+
+
+
+
+
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
 
 
 
