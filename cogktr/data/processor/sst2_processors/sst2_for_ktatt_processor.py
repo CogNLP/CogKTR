cfb7c1c88f54dc1d7173ba8ffae1a5260ec5a58d
CrocodileZS <zhouyuyang17@163.com>
2022-07-21 12:04:31 +0800

    update kt-att

--- cogktr/data/processor/sst2_processors/sst2_for_ktatt_processor.py
+++ cogktr/data/processor/sst2_processors/sst2_for_ktatt_processor.py
@@ -1,47 +1,119 @@
-from cogktr import Sst2Reader
-from cogktr.enhancers.linker.wikipedia_linker import WikipediaLinker
-from cogktr.enhancers.searcher.wikipedia_searcher import WikipediaSearcher
+from cogktr.data.reader.sst2_reader import Sst2Reader
 from cogktr.data.datable import DataTable
-from cogktr.data.datableset import DataTableSet
 from tqdm import tqdm
-from cogktr.data.processor.sst2_processors import Sst2Processor
+from cogktr.enhancers.world_enhancer import WorldEnhancer
+from transformers import BertTokenizer
+from cogktr.data.processor.base_processor import BaseProcessor
+import numpy as np
+from cogktr.data.datableset import DataTableSet
+import os
 
 
-class Sst2ForKtattProcessor(Sst2Processor):
-    def __init__(self, plm, max_token_len, vocab, knowledge_path):
-        super().__init__(plm, max_token_len, vocab)
-        self.knowledge_path = knowledge_path
+class Sst2ForKtattProcessor(BaseProcessor):
+    def __init__(self, plm, max_token_len, vocab):
+        super().__init__()
+        self.plm = plm
+        self.max_token_len = max_token_len
+        self.vocab = vocab
+        self.tokenizer = BertTokenizer.from_pretrained(plm)
 
-    def process_train(self, data):
+    def _process(self, data, enhanced_dict=None):
         datable = DataTable()
-        print("Integrating knowledge text and processing data...")
+        print("Processing data...")
+
         for sentence, label in tqdm(zip(data['sentence'], data['label']), total=len(data['sentence'])):
-            sentence = self.integrate_kt(sentence, self.knowledge_path)
-            token = self.tokenizer.encode(text=sentence, truncation=True, padding="max_length", add_special_tokens=True,
-                                          max_length=self.max_token_len)
-            datable("token", token)
+            token_ids = self.tokenizer.encode(enhanced_dict[sentence]['words'])
+            attention_mask = np.zeros((self.max_token_len, self.max_token_len))
+            lst_pos = len(token_ids)
+            attention_mask[0:lst_pos, 0:lst_pos] = 1
+
+            entities = enhanced_dict[sentence]['entities']
+            for entity in entities:
+                if isinstance(entity['desc'], str):
+                    entity_desc_token_ids = self.tokenizer.encode(entity['desc'], add_special_tokens=False)
+                    curr_lst_pos = min(lst_pos+len(entity_desc_token_ids), self.max_token_len)
+                    attention_mask[lst_pos:curr_lst_pos, lst_pos:curr_lst_pos] = 1
+                    attention_mask[entity['start']+1:entity['end']+1, lst_pos:curr_lst_pos] = 1
+                    attention_mask[lst_pos:curr_lst_pos, entity['start']+1:entity['end']+1] = 1
+                    lst_pos = curr_lst_pos
+                    if lst_pos <= self.max_token_len:
+                        token_ids = token_ids + entity_desc_token_ids
+                    else:
+                        token_ids = (token_ids + entity_desc_token_ids)[0:self.max_token_len]
+                        break
+            if lst_pos < self.max_token_len:
+                token_ids = token_ids + [0]*(self.max_token_len-lst_pos)
+
+            datable("input_ids", token_ids)
+            datable("attention_mask", attention_mask)
             datable("label", self.vocab["label_vocab"].label2id(label))
         return DataTableSet(datable)
 
-    @staticmethod
-    def integrate_kt(sentence, knowledge_path, linker_tool="tagme", searcher_tool="blink"):
-        linker = WikipediaLinker(tool=linker_tool)
-        searcher = WikipediaSearcher(tool=searcher_tool, path=knowledge_path)
-        entity_ids = [entity["id"] for entity in linker.link(sentence).values()]
-        for entity_id in entity_ids:
-            sentence = sentence + searcher.search(entity_id)[entity_id]['desc']
-        return sentence
+    def process_train(self, data,enhanced_dict=None):
+        return self._process(data, enhanced_dict)
+
+    def process_dev(self, data, enhanced_dict=None):
+        return self._process(data, enhanced_dict)
+
+    def process_test(self, data, enhanced_dict=None):
+        datable = DataTable()
+        print("Processing data...")
+
+        for sentence in tqdm(zip(data['sentence']), total=len(data['sentence'])):
+            token_ids = self.tokenizer.encode(enhanced_dict[sentence]['words'])
+            attention_mask = np.zeros((self.max_token_len, self.max_token_len))
+            lst_pos = len(token_ids)
+            attention_mask[0:lst_pos, 0:lst_pos] = 1
+
+            entities = enhanced_dict[sentence]['entities']
+            for entity in entities:
+                if isinstance(entity['desc'], str):
+                    entity_desc_token_ids = self.tokenizer.encode(entity['desc'], add_special_tokens=False)
+                    curr_lst_pos = min(lst_pos + len(entity_desc_token_ids), self.max_token_len)
+                    attention_mask[lst_pos:curr_lst_pos, lst_pos:curr_lst_pos] = 1
+                    attention_mask[entity['start'] + 1:entity['end'] + 1, lst_pos:curr_lst_pos] = 1
+                    attention_mask[lst_pos:curr_lst_pos, entity['start'] + 1:entity['end'] + 1] = 1
+                    lst_pos = curr_lst_pos
+                    if lst_pos <= self.max_token_len:
+                        token_ids = token_ids + entity_desc_token_ids
+                    else:
+                        token_ids = (token_ids + entity_desc_token_ids)[0:self.max_token_len]
+                        break
+            if lst_pos < self.max_token_len:
+                token_ids = token_ids + [0]*(self.max_token_len-lst_pos)
+
+            datable("input_ids", token_ids)
+            datable("attention_mask", attention_mask)
+        return DataTableSet(datable)
 
 
 if __name__ == "__main__":
+    # os.environ["CUDA_VISIBLE_DEVICES"] = '1'
+    # os.environ["CUDA_LAUNCH_BLOCKING"] = '1'
     # sentence = KTSST2Processor.integrate_kt(sentence="Bert likes reading in the library.",
     #                                        knowledge_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/knowledge_graph/wikipedia_desc/entity.jsonl")
     reader = Sst2Reader(raw_data_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/text_classification/SST_2/raw_data")
     train_data, dev_data, test_data = reader.read_all()
     vocab = reader.read_vocab()
-    processor = Sst2ForKtattProcessor(plm="bert-base-cased", max_token_len=128, vocab=vocab,
-                                knowledge_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/knowledge_graph/wikipedia_desc/entity.jsonl")
-    train_dataset = processor.process_train(train_data)
-    dev_dataset = processor.process_dev(dev_data)
-    test_dataset = processor.process_test(test_data)
+
+    enhancer = WorldEnhancer(knowledge_graph_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/knowledge_graph",
+                             cache_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/text_classification/SST_2/enhanced_data",
+                             cache_file="world_data",
+                             reprocess=False,
+                             load_entity_desc=True,
+                             load_entity_embedding=False,
+                             load_entity_kg=False)
+    # enhanced_train_dict = enhancer.enhance_train(train_data)
+    enhanced_dev_dict = enhancer.enhance_dev(datable=dev_data,
+                                             enhanced_key_1="sentence",
+                                             return_entity_desc=True,
+                                             return_entity_embedding=False,
+                                             return_entity_kg=False)
+    # enhanced_test_dict = enhancer.enhance_test(test_data)
+    # print("finish enhancer")
+
+    processor = Sst2ForKtattProcessor(plm="bert-base-cased", max_token_len=128, vocab=vocab)
+    # train_dataset = processor.process_train(train_data)
+    dev_dataset = processor.process_dev(data=dev_data, enhanced_dict=enhanced_dev_dict)
+    # test_dataset = processor.process_test(test_data)
     print("end")
--- cogktr/enhancers/world_enhancer.py
+++ cogktr/enhancers/world_enhancer.py
@@ -6,7 +6,7 @@ from cogktr.enhancers.linker.wikipedia_linker import WikipediaLinker
 from cogktr.enhancers.embedder.wikipedia_embedder import WikipediaEmbedder
 from cogktr.enhancers.searcher.wikipedia_searcher import WikipediaSearcher
 from cogktr.enhancers.searcher.wikidata_searcher import WikidataSearcher
-from cogktr.utils.io_utils import save_json, load_json
+from cogktr.utils.io_utils import save_pickle, load_pickle
 from tqdm import tqdm
 
 
@@ -114,76 +114,73 @@ class WorldEnhancer(BaseEnhancer):
                       return_entity_kg=False):
         enhanced_dict = {}
         if not self.reprocess and os.path.exists(os.path.join(self.cache_path_file, dict_name)):
-            enhanced_dict = load_json(enhanced_dict)
+            enhanced_dict = load_pickle(os.path.join(self.cache_path_file, dict_name))
         else:
             print("Enhancing data...")
             if enhanced_key_2 is None:
                 for sentence in tqdm(datable[enhanced_key_1]):
                     enhanced_sentence_dict = self.enhance_sentence(sentence=sentence,
-                                                                   return_entity_desc=False,
-                                                                   return_entity_embedding=False,
-                                                                   return_entity_kg=False)
+                                                                   return_entity_desc=return_entity_desc,
+                                                                   return_entity_embedding=return_entity_embedding,
+                                                                   return_entity_kg=return_entity_kg)
                     enhanced_dict.update(enhanced_sentence_dict)
-                save_json(enhanced_dict, os.path.join(self.cache_path_file, dict_name))
+                save_pickle(enhanced_dict, os.path.join(self.cache_path_file, dict_name))
             else:
                 for sentence, sentence_pair in tqdm(zip(datable[enhanced_key_1], datable[enhanced_key_2]),
                                                     total=len(datable[enhanced_key_1])):
                     enhanced_sentence_pair = self.enhance_sentence_pair(sentence=sentence,
                                                                         sentence_pair=sentence_pair,
-                                                                        return_entity_desc=False,
-                                                                        return_entity_embedding=False,
-                                                                        return_entity_kg=False)
+                                                                        return_entity_desc=return_entity_desc,
+                                                                        return_entity_embedding=return_entity_embedding,
+                                                                        return_entity_kg=return_entity_kg)
                     enhanced_dict.update(enhanced_sentence_pair)
-                save_json(enhanced_dict, os.path.join(self.cache_path_file, dict_name))
+                save_pickle(enhanced_dict, os.path.join(self.cache_path_file, dict_name))
         return enhanced_dict
 
     def enhance_train(self,
                       datable,
                       enhanced_key_1="sentence",
                       enhanced_key_2=None,
-                      return_ner=False,
-                      return_srl=False,
-                      return_syntax=False,
-                      return_wordnet=False):
+                      return_entity_desc=False,
+                      return_entity_embedding=False,
+                      return_entity_kg=False):
         return self._enhance_data(datable=datable,
-                                  dict_name="enhanced_train.json",
-                                  enhanced_key_1=None,
-                                  enhanced_key_2=None,
-                                  return_entity_desc=False,
-                                  return_entity_embedding=False,
-                                  return_entity_kg=False)
+                                  dict_name="enhanced_train.pkl",
+                                  enhanced_key_1=enhanced_key_1,
+                                  enhanced_key_2=enhanced_key_2,
+                                  return_entity_desc=return_entity_desc,
+                                  return_entity_embedding=return_entity_embedding,
+                                  return_entity_kg=return_entity_kg)
 
     def enhance_dev(self,
                     datable,
                     enhanced_key_1="sentence",
                     enhanced_key_2=None,
-                    return_ner=False,
-                    return_srl=False,
-                    return_syntax=False,
-                    return_wordnet=False):
+                    return_entity_desc=False,
+                    return_entity_embedding=False,
+                    return_entity_kg=False):
         return self._enhance_data(datable=datable,
-                                  dict_name="enhanced_dev.json",
-                                  enhanced_key_1=None,
-                                  enhanced_key_2=None,
-                                  return_entity_desc=False,
-                                  return_entity_embedding=False,
-                                  return_entity_kg=False)
+                                  dict_name="enhanced_dev.pkl",
+                                  enhanced_key_1=enhanced_key_1,
+                                  enhanced_key_2=enhanced_key_2,
+                                  return_entity_desc=return_entity_desc,
+                                  return_entity_embedding=return_entity_embedding,
+                                  return_entity_kg=return_entity_kg)
 
     def enhance_test(self,
                      datable,
                      enhanced_key_1="sentence",
                      enhanced_key_2=None,
-                     return_ner=False,
-                     return_srl=False,
-                     return_syntax=False,
-                     return_wordnet=False):
+                     return_entity_desc=False,
+                     return_entity_embedding=False,
+                     return_entity_kg=False):
         return self._enhance_data(datable=datable,
-                                  dict_name="enhanced_test.json",
-                                  enhanced_key_1=None,
-                                  enhanced_key_2=None,
-                                  return_entity_desc=False,
-                                  return_entity_embedding=False,
-                                  return_entity_kg=False)
+                                  dict_name="enhanced_test.pkl",
+                                  enhanced_key_1=enhanced_key_1,
+                                  enhanced_key_2=enhanced_key_2,
+                                  return_entity_desc=return_entity_desc,
+                                  return_entity_embedding=return_entity_embedding,
+                                  return_entity_kg=return_entity_kg)
 
 if __name__ == "__main__":
     from cogktr.data.reader.sst2_reader import Sst2Reader
@@ -191,43 +188,42 @@ if __name__ == "__main__":
     enhancer = WorldEnhancer(knowledge_graph_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/knowledge_graph",
                  cache_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/text_classification/SST_2/enhanced_data",
                  cache_file="world_data",
-                 reprocess=True,
+                 reprocess=False,
                  load_entity_desc=True,
-                 load_entity_embedding=True,
+                 load_entity_embedding=False,
                  load_entity_kg=False)
 
     enhanced_sentence_dict_1 = enhancer.enhance_sentence(sentence="Bert likes reading in the Sesame Street Library.",
                                                          return_entity_desc=True,
-                                                         return_entity_embedding=True,
+                                                         return_entity_embedding=False,
                                                          return_entity_kg=False)
+    #
+    # enhanced_sentence_dict_2 = enhancer.enhance_sentence(sentence=["Bert", "likes", "reading", "in the", "Street"],
+    #                                                      return_entity_desc=True,
+    #                                                      return_entity_embedding=True,
+    #                                                      return_entity_kg=False)
 
-    enhanced_sentence_dict_2 = enhancer.enhance_sentence(sentence=["Bert", "likes", "reading", "in the", "Street"],
-                                                         return_entity_desc=True,
-                                                         return_entity_embedding=True,
-                                                         return_entity_kg=False)
+    # enhanced_sentence_dict_3 = enhancer.enhance_sentence_pair(
+    #     sentence="Bert likes reading in the Sesame Street Library.",
+    #     sentence_pair="Bert likes reading.",
+    #     return_entity_desc=True,
+    #     return_entity_embedding=True,
+    #     return_entity_kg=False)
+    #
+    # enhanced_sentence_dict_4 = enhancer.enhance_sentence_pair(
+    #     sentence=["Bert", "likes", "reading", "in the", "Street"],
+    #     sentence_pair=["Bert", "likes", "reading"],
+    #     return_entity_desc=True,
+    #     return_entity_embedding=True,
+    #     return_entity_kg=False)
 
-    enhanced_sentence_dict_3 = enhancer.enhance_sentence_pair(
-        sentence="Bert likes reading in the Sesame Street Library.",
-        sentence_pair="Bert likes reading.",
-        return_entity_desc=True,
-        return_entity_embedding=True,
-        return_entity_kg=False)
-
-    enhanced_sentence_dict_4 = enhancer.enhance_sentence_pair(
-        sentence=["Bert", "likes", "reading", "in the", "Street"],
-        sentence_pair=["Bert", "likes", "reading"],
-        return_entity_desc=True,
-        return_entity_embedding=True,
-        return_entity_kg=False)
-
-    reader_5 = Sst2Reader(raw_data_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/text_classification/SST_2/raw_data")
-    train_data_5, dev_data_5, test_data_5 = reader_5.read_all()
-    enhanced_dev_dict_5 = enhancer.enhance_dev(datable=dev_data_5,
-                                               enhanced_key_1="sentence",
-                                               return_ner=True,
-                                               return_srl=True,
-                                               return_syntax=True,
-                                               return_wordnet=True)
+    # reader_5 = Sst2Reader(raw_data_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/text_classification/SST_2/raw_data")
+    # train_data_5, dev_data_5, test_data_5 = reader_5.read_all()
+    # enhanced_dev_dict_5 = enhancer.enhance_dev(datable=dev_data_5,
+    #                                            enhanced_key_1="sentence",
+    #                                            return_entity_desc=True,
+    #                                            return_entity_embedding=False,
+    #                                            return_entity_kg=False)
 
     # reader_6 = QnliReader(raw_data_path="/home/chenyuheng/zhouyuyang/CogKTR/datapath/sentence_pair/QNLI/raw_data")
     # train_data_6, dev_data_6, test_data_6 = reader_6.read_all()
