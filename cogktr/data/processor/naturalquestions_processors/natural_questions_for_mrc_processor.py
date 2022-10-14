a048876099f443d38a533726d6e4f2b8f765e84d
HarryPotter <1208314139@qq.com>
2022-10-14 14:15:10 +0800

    update natural questions reader and processor

--- cogktr/data/processor/naturalquestions_processors/natural_questions_for_mrc_processor.py
+++ cogktr/data/processor/naturalquestions_processors/natural_questions_for_mrc_processor.py
@@ -8,6 +8,18 @@ from argparse import Namespace
 from cogktr.data.processor.squad2_processors.squad2_processor import prepare_mrc_input
 transformers.logging.set_verbosity_error()  # set transformers logging level
 
+import re
+import torch
+import collections
+from torch._six import string_classes
+np_str_obj_array_pattern = re.compile(r'[SaUO]')
+
+default_collate_err_msg_format = (
+    "default_collate: batch must contain tensors, numpy arrays, numbers, "
+    "dicts or lists; found {}")
+
+
+
 class NaturalQuestionsForMRCProcessor(BaseProcessor):
     def __init__(self, plm, max_token_len):
         super().__init__()
@@ -39,8 +51,9 @@ class NaturalQuestionsForMRCProcessor(BaseProcessor):
                 example = Namespace(**{"question_text":question,
                                        "doc_tokens":doc_tokens,
                                        "qas_id":str(question_id)+'-'+str(psg_id),
-                                       "answers":{"text":answer for answer in answers},
+                                       "answers":[{'text':answer} for answer in answers],
                                        "is_impossible":False,
+                                       "relavance_score":psg[2],
                                        })
                 results = prepare_mrc_input(example,
                                             tokenizer=self.tokenizer,
@@ -65,20 +78,62 @@ class NaturalQuestionsForMRCProcessor(BaseProcessor):
     def process_test(self,data,enhanced_data_dict):
         return self._process(data,enhanced_data_dict)
 
-    # def process_test(self, data):
-    #     datable = DataTable()
-    #     print("Processing data...")
-    #     for sentence in tqdm(zip(data['sentence']), total=len(data['sentence'])):
-    #         token = self.tokenizer.encode(text=sentence, truncation=True, padding="max_length", add_special_tokens=True,
-    #                                       max_length=self.max_token_len)
-    #         tokenized_data = self.tokenizer.encode_plus(text=sentence,
-    #                                                     padding="max_length",
-    #                                                     add_special_tokens=True,
-    #                                                     max_length=self.max_token_len)
-    #         datable("input_ids", tokenized_data["input_ids"])
-    #         datable("token_type_ids", tokenized_data["token_type_ids"])
-    #         datable("attention_mask", tokenized_data["attention_mask"])
-    #     return DataTableSet(datable)
+
+    def _collate(self, batch):
+        elem = batch[0]
+        elem_type = type(elem)
+        if isinstance(elem, torch.Tensor):
+            out = None
+            if torch.utils.data.get_worker_info() is not None:
+                # If we're in a background process, concatenate directly into a
+                # shared memory tensor to avoid an extra copy
+                numel = sum(x.numel() for x in batch)
+                storage = elem.storage()._new_shared(numel)
+                out = elem.new(storage).resize_(len(batch), *list(elem.size()))
+            return torch.stack(batch, 0, out=out)
+        elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+                and elem_type.__name__ != 'string_':
+            if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+                # array of string classes and object
+                if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+                    raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+                return self._collate([torch.as_tensor(b) for b in batch])
+            elif elem.shape == ():  # scalars
+                return torch.as_tensor(batch)
+        elif isinstance(elem, float):
+            return torch.tensor(batch, dtype=torch.float64)
+        elif isinstance(elem, int):
+            return torch.tensor(batch)
+        elif isinstance(elem, string_classes):
+            return batch
+        elif isinstance(elem, collections.abc.Mapping):
+            try:
+                return elem_type({key: self._collate([d[key] for d in batch]) for key in elem})
+            except TypeError:
+                # The mapping type may not support `__init__(iterable)`.
+                return {key: self._collate([d[key] for d in batch]) for key in elem}
+        elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+            return elem_type(*(self._collate(samples) for samples in zip(*batch)))
+        elif isinstance(elem, collections.abc.Sequence):
+            # check to make sure that the elements in batch have consistent size
+            it = iter(batch)
+            elem_size = len(next(it))
+            if not all(len(elem) == elem_size for elem in it):
+                raise RuntimeError('each element in list of batch should be of equal size')
+            transposed = list(zip(*batch))  # It may be accessed twice, so we use a list.
+
+            if isinstance(elem, tuple):
+                return [self._collate(samples) for samples in transposed]  # Backwards compatibility.
+            else:
+                try:
+                    return elem_type([self._collate(samples) for samples in transposed])
+                except TypeError:
+                    # The sequence type may not support `__init__(iterable)` (e.g., `range`).
+                    return [self._collate(samples) for samples in transposed]
+
+        elif isinstance(elem,Namespace):
+            return batch
 
 def _is_whitespace(c):
     if c == " " or c == "\t" or c == "\r" or c == "\n" or ord(c) == 0x202F:
